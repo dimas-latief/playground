5d8915aa4dbc6d4be519b51e8c67c11319c27634
muhammadLatief <latief@codapayments.com>
2019-10-15 19:09:53 +0700

    Adding group customer and exception if not match

--- no3.py
+++ no3.py
@@ -4,9 +4,14 @@ class Customer():
         '''the constructor when creating the customer
         something like the minimum requirement of creating
         the customer'''
-        
+
         self.name = name
         self.group = group.lower()
         self.created_since_in_years = created_since_in_years
         # we need to throw error if not match with the list of the customer group
         print(f"Customer object created with name {self.name} within group {self.group} and join since {self.created_since_in_years} years ago.")
+        
+        list_group = ("employee", "affiliate", "customer")
+        if (self.group not in list_group):
+            raise Exception('I am sorry, the group is not in the list.')
+
