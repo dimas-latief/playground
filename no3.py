096971a5a6c23d4c368078d40a22ce089d2a507f
muhammadLatief <latief@codapayments.com>
2019-10-15 19:08:43 +0700

    Create Customer Class with constuctor name, group and createdAt

---
+++ no3.py
@@ -0,0 +1,12 @@
+class Customer():
+    '''this is where we create new customer created'''
+    def __init__(self, name, group, created_since_in_years = 2):
+        '''the constructor when creating the customer
+        something like the minimum requirement of creating
+        the customer'''
+        
+        self.name = name
+        self.group = group.lower()
+        self.created_since_in_years = created_since_in_years
+        # we need to throw error if not match with the list of the customer group
+        print(f"Customer object created with name {self.name} within group {self.group} and join since {self.created_since_in_years} years ago.")
