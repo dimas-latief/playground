935f7ce868cc33cee9f3f635ec633aa5aae03731
muhammadLatief <latief@codapayments.com>
2019-10-15 18:57:51 +0700

    constant big O analyis

--- no1.py
+++ no1.py
@@ -5,6 +5,6 @@ def store(a):
     '''
     Store function
     input params: a dict. example {"key1":"value1"}
-    Big(O) analyysis: O(1) --> its constant
+    Big(O) analysis: O(1) --> its constant
     '''
     text.update(a) # using update funtion to add new dict
