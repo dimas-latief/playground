932ca2ccee927de8a5c220d87fb14bae5b3f4b77
muhammadLatief <latief@codapayments.com>
2019-10-15 18:56:07 +0700

    Store function with update method to add new dictionary

--- no1.py
+++ no1.py
@@ -1,2 +1,9 @@
 # global variable define as dictionary
 text = {}
+
+def store(a):
+    '''
+    Store function
+    input params: a dict. example {"key1":"value1"}
+    '''
+    text.update(a) # using update funtion to add new dict
