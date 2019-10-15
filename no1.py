2f7e8befe70046d6b3104de99db51e4b54265fa8
muhammadLatief <latief@codapayments.com>
2019-10-15 18:58:53 +0700

    Load function, return new form of the data

--- no1.py
+++ no1.py
@@ -8,3 +8,16 @@ def store(a):
     Big(O) analysis: O(1) --> its constant
     '''
     text.update(a) # using update funtion to add new dict
+
+def load(a):
+    '''
+    load function
+    params: what object we want to get, in this case is text dict object
+    return: string type format
+    In here n is the lenght of the dictionary
+    '''
+    new_form = "" # init empty string
+    for k,v in a.items(): # unpackage the tuple to have the key and value
+        new_form += k +"="+v+";\n" # append string
+    print(new_form) # printing out
+    return new_form
