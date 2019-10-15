4be8108644046e0daabb7cff1f4ac1c3b989da3f
muhammadLatief <latief@codapayments.com>
2019-10-15 20:23:13 +0700

    modified print format

--- no3/customer.py
+++ no3/customer.py
@@ -8,12 +8,15 @@ class Customer():
         self.name = name
         self.group = group.lower()
         self.created_since_in_years = created_since_in_years
-        # we need to throw error if not match with the list of the customer group
-        print(f"Customer object created with name {self.name} within group {self.group} and join since {self.created_since_in_years} years ago.")
         
         list_group = ("employee", "affiliate", "customer")
+
+        # we need to throw error if not match with the list of the customer group
         if (self.group not in list_group):
             raise Exception('I am sorry, the group is not in the list.')
+
+        print("Customer object created with name {} within group {} and join since {} years ago".format(self.name, self.group, self.created_since_in_years))
+
 
     def check_discount_rules(self):
         '''every group have different discount'''
